86d12df9669df89d45e55642de1aec466e0e8653
Boluchel <boluchel123@gmail.com>
2024-06-09 06:51:42 +0100

    feat: added the create and mint assets actions

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Boluwatife"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Chelsea"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "Bolu"
 )
 
 var ID ids.ID
